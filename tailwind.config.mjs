3213c042ab2317d7ab8de9b366ae65cadeb5ec95
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2025-01-07 22:33:09 +0800

    Update deps

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        light: '#888888', // Text color
-        mainBg: '#031716', // Large main background
-        accentBg: '#032F30', // Large accent background
-        primary: '#0A7075', // Primary color
-        primaryAccent: '#0C969C', // Primary accent
-        secondary: '#6BA3BE', // Secondary color
-        secondaryAccent: '#274D60', // Secondary accent
-      },
-    },
+  	extend: {
+  		colors: {
+  			light: '#888888',
+  			mainBg: '#031716',
+  			accentBg: '#032F30',
+  			primary: '#0A7075',
+  			primaryAccent: '#0C969C',
+  			secondary: '#6BA3BE',
+  			secondaryAccent: '#274D60'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
